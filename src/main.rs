b08d4d6f2c9d20c7f144e85e2087a4e32253f8df
NotAShelf <raf@notashelf.dev>
2024-01-04 10:39:18 +0300

    make host and port configurable

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,45 @@
+extern crate clap;
 extern crate rouille;
 
 use rand::Rng;
 use rouille::router;
 use std::fs::File;
 use std::io::Read;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Opts {
+   #[clap(short = 'i', long)]
+   ip: String,
+   #[clap(short = 'p', long)]
+   port: String,
+}
 
 fn get_random_element<T>(vector: &Vec<T>) -> &T {
-    let mut rng = rand::thread_rng();
-    let random_index = rng.gen_range(0..vector.len());
-    &vector[random_index]
+   let mut rng = rand::thread_rng();
+   let random_index = rng.gen_range(0..vector.len());
+   &vector[random_index]
 }
 
 fn main() {
-    let mut json = String::new();
-    File::open(std::env::var("SEARX_INSTANCES").expect("missing SEARX_INSTANCES"))
-        .expect("Failed to open file")
-        .read_to_string(&mut json)
-        .expect("Failed to read file");
+   let opts = Opts::parse();
 
-    let engines: Vec<String> = serde_json::from_str(&json).expect("Failed to deserialize JSON");
+   let mut json = String::new();
+   File::open(std::env::var("SEARX_INSTANCES").expect("missing SEARX_INSTANCES"))
+       .expect("Failed to open file")
+       .read_to_string(&mut json)
+       .expect("Failed to read file");
 
-    println!("Now listening on 127.0.0.1:8000");
+   let engines: Vec<String> = serde_json::from_str(&json).expect("Failed to deserialize JSON");
 
-    rouille::start_server("127.0.0.1:8000", move |request| {
-        router!(request,
-            (GET) (/search) => {
-                rouille::Response::redirect_302(format!("https://{}/search?{}", get_random_element(&engines), &request.raw_query_string()))
+   println!("Now listening on {}", format!("{}:{}", opts.ip, opts.port));
 
-            },
-            _ => rouille::Response::empty_404()
-        )
-    });
+   rouille::start_server(format!("{}:{}", opts.ip, opts.port), move |request| {
+       router!(request,
+           (GET) (/search) => {
+               rouille::Response::redirect_302(format!("https://{}/search?{}", get_random_element(&engines), &request.raw_query_string()))
+           },
+           _ => rouille::Response::empty_404()
+       )
+   });
 }
